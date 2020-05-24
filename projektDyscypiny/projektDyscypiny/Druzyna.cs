fa5baae99587b34c1fcf9a54bc97345b5044a522
werooonisian <65867091+werooonisian@users.noreply.github.com>
2020-05-24 21:30:01 +0200

    dodanie klasy druzyna

---
+++ projektDyscypiny/projektDyscypiny/Druzyna.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektDyscypiny
+{
+    class Druzyna
+    {
+        private int idDruzyna;
+        private String nazwaDruzyny = "";
+        private int punkty;
+        private static int iloscDruzyn; // ?? ma być statyczna
+        private int wygrane; // (iloscWygranych) trzeba było zmienić bo powtarza się z metodą
+        private int przegrane;// (iloscPrzegranych)
+
+        public Druzyna(String nazwaDruzyny) { }
+        public int getPunkty() { return punkty; }
+        public int getID_Druzyna() { return idDruzyna; }
+        public String getNazwaDruzyny() { return nazwaDruzyny; }
+        public int setPunkty() { return 1; } // do napisania
+        public int iloscWygranych() { return 2; } //do napisania/ może zmiana nazwy
+        public int iloscPrzegranyc() { return 3; } //do napisania/ może zmiana nazwy
+    }
+}
